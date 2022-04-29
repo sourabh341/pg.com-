7032795823c8dd414206d3796e02c45c917a2a32
sourabh341 <sourabhsharmaji988@gmail.com>
2022-04-29 20:15:19 +0530

    display flex

--- script.js
+++ script.js
@@ -2,7 +2,35 @@ function hello(){
     alert("not available");
 }
 
-function value(){
-    alert("sourabbh");
+function login(){
+  
+    var username = document.getElementById("user_name").value;
+    var useremail = document.getElementById("user_email").value;
+    if(username.length != 0 && useremail.length != 0){
+        alert("logined in ");
+        return ;
+    }
+    else{
+        alert("failed");
+    }
+    
 }
+function memberLogin(){
+    var memberName = document.getElementById("mem-name").value;
+    var memberMObileNumber = document.getElementById("mem_mobile_number").value;
+    var memberAddress = document.getElementById("member_address").value;
+    var memberImage = document.getElementById("room_img").value;
+    if(memberName!=0 && memberAddress!=0 ){
+            alert("your detail will soon display here ");
+    }
+    if(memberMObileNumber==8 && memberImage==ture ){
+        alert("faild")
+    }
+    else
+    {
+        alert("failed");
+    }
+    
+} 
+
 
